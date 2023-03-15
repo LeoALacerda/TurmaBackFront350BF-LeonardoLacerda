85dc28af202a0a0edda4587a7c2393ed51f929e6
Leonardo Lacerda <leoarlac@gmail.com>
2023-03-15 12:44:48 -0400

    exercicio de struct finalizado

--- Struct/Struct/main.swift
+++ Struct/Struct/main.swift
@@ -28,3 +28,58 @@ caio.nome = "Alfredo"
 
 print(caio.nome)
 print(matheus.nome)
+
+//MARK: EXERCICIO STRUCT VEICULOS
+//Definir uma classe Veículo com cor, preço e quantidade de passageiros, depois defina as classes Carro que tem tamanho das rodas, e Avião que tem piloto e companhia aérea. queremos descobrir a quantidade de combustível que cada um dos veículos gasta por viagem, defina um método que recebe como parâmetro a distancia em Km e calcule a quantidade de combustível. Carro: tamanho das rodas * quantidade de passageiros * distancia
+//Avião quantidade de passageiros * distancia
+
+class Vehicle{
+    var color: String
+    var price: Double
+    var passengers: Int
+    
+    init(color: String, price: Double, passengers: Int) {
+        self.color = color
+        self.price = price
+        self.passengers = passengers
+    }
+    
+    func fuelCalculator(distanceKM: Double){
+        print("Something went wrong!")
+    }
+}
+
+class Car: Vehicle{
+    var tireSize: Double
+    
+    init(tireSize: Double, color: String, price: Double, passengers: Int) {
+        self.tireSize = tireSize
+        super.init(color: color, price: price, passengers: passengers)
+    }
+    
+    override func fuelCalculator(distanceKM: Double){
+        print ("Consumed Fuel: \(tireSize * Double(passengers) * distanceKM)L")
+    }
+}
+
+class Airplane: Vehicle{
+    var pilot: String
+    var airCompany: String
+    
+    init(pilot: String, airCompany: String, color: String, price: Double, passengers: Int) {
+        self.pilot = pilot
+        self.airCompany = airCompany
+        super.init(color: color, price: price, passengers: passengers)
+    }
+    
+    override func fuelCalculator(distanceKM: Double){
+        print ("Consumed Fuel: \(Double(passengers) * distanceKM)L")
+    }
+}
+
+var myCar: Car = Car(tireSize: 17, color: "Black", price: 120000, passengers: 5)
+myCar.fuelCalculator(distanceKM: 100)
+var myVehicle: Vehicle = Vehicle(color: "Red", price: 200000, passengers: 3)
+myVehicle.fuelCalculator(distanceKM: 100)
+var myAirplane: Airplane = Airplane(pilot: "Me", airCompany: "Delta", color: "White", price: 13000000, passengers: 500)
+myAirplane.fuelCalculator(distanceKM: 700)
