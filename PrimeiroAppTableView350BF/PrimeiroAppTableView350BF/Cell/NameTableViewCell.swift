f9bc25f52b1575dd8ec3bae4238c0b9f51bf5e73
Leonardo Lacerda <leoarlac@gmail.com>
2023-04-11 21:38:46 -0300

    criando celula e objeto para exibir na xib

--- PrimeiroAppTableView350BF/PrimeiroAppTableView350BF/Cell/NameTableViewCell.swift
+++ PrimeiroAppTableView350BF/PrimeiroAppTableView350BF/Cell/NameTableViewCell.swift
@@ -7,14 +7,35 @@
 
 import UIKit
 
+// MARK: - Observação: O IDENTIFIER deve ter o MESMO NOME DO FILE (nome da classe, do arquivo swift, do arquivo storyboard, nome do identificador tudo igual!)
+// MARK: - Criar setup
+
 class NameTableViewCell: UITableViewCell {
 
     @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var surnameLabel: UILabel!
+    
+    static func test(){
+        print(identifier)
+    }
+    
+    //static: Significa algo que nao tem nenhuma dependencia externa!
+    static let identifier: String = "NameTableViewCell"
+    
+    static func nib() -> UINib{
+        return UINib(nibName: self.identifier, bundle: nil)
+    }
     
     // Ciclo de vida de uma célula, igual ao viewDidLoad
     override func awakeFromNib() {
         super.awakeFromNib()
         
+    }
+    
+    func setupCell(person: Person) {
+        nameLabel.text = person.name
+        surnameLabel.text = person.surname
     }
     
 }
--- PrimeiroAppTableView350BF/PrimeiroAppTableView350BF/ViewController.swift
+++ PrimeiroAppTableView350BF/PrimeiroAppTableView350BF/ViewController.swift
@@ -11,11 +11,23 @@ import UIKit
 
 // 1o: Criar a tableView e fazer a sua ligação
 // 2o: Configurar a TableView (delegate e dataSource)
-// 3o: Criar célula customizada
+// 3o: Configurar os protocolos
+// 4o: Criar célula customizada
+// 5o: Registrar célula
+// 6o: Configurar os métodos da TableView
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var nameList: [String] = ["Barbara", "Bernardo","Bruno", "Douglas", "Leo"]
+    var surnameList: [String] = ["1", "2","3", "4", "Lacerda"]
+    
+    var personList: [Person] = [Person(name: "Barbara", surname: "Teste 1"),
+                                Person(name: "Bernardo", surname: "2"),
+                                Person(name: "Bruno", surname: "3"),
+                                Person(name: "Douglas", surname: "4"),
+                                Person(name: "Leo", surname: "Lacerda")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,22 +38,31 @@ class ViewController: UIViewController {
         // 3o: Configurar os protocolos
         tableView.delegate = self
         tableView.dataSource = self
-        // 4o: Registrar a célula
-        //tableView.register(<#T##nib: UINib?##UINib?#>, forCellReuseIdentifier: <#T##String#>)
+        // 5o: Registrar a célula
+        tableView.register(NameTableViewCell.nib(), forCellReuseIdentifier: NameTableViewCell.identifier)
     }
+
     
 }
 // DATASOURCE -> Responsável pela parte de DADOS
 extension ViewController: UITableViewDataSource{
     
+    // 6o: Configurar os métodos da TableView
+    
     // Quantidade de itens por SEÇÃO
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return nameList.count
     }
     
-    // Quem da a vida a célula
+    // Responsável por criar as células, dar vida a elas
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: NameTableViewCell.identifier, for: indexPath) as? NameTableViewCell
+        cell?.setupCell(person: personList[indexPath.row])
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 83
     }
     
     
@@ -51,3 +72,22 @@ extension ViewController: UITableViewDataSource{
 extension ViewController: UITableViewDelegate{
     
 }
+
+
+// static serve para vc nao precisar instanciar uma variavel/funcao!
+// ele serve para vc economizar memoria
+
+//class Pessoa{
+//    static var caio: String = "Caio"
+//}
+//
+//var caio1 = Pessoa.caio
+//var caio2 = Pessoa.caio
+// Não precisa instanciar Pessoa para acessar a variável caio.
+// ou seja, não precisa dos parenteses: var caio1 = Pessoa().caio
+
+
+//
+// o _ serve para o valor1 ficar implicito
+// test (String)
+// func test (_ valor1: String){}
