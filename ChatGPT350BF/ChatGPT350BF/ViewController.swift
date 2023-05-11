903bd7ac5152938dc20e1e50e5168b65868a3713
Leonardo Lacerda <leoarlac@gmail.com>
2023-05-11 19:44:33 -0300

    criado o projeto do chatGPT

---
+++ ChatGPT350BF/ChatGPT350BF/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatGPT350BF
+//
+//  Created by Leonardo Lacerda on 11/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
